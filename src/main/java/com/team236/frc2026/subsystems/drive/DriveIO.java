607136a3aaa7268de35c0eaa7d607ab8e39f0a1e
OwenGarabedian <owengarabedian@gmail.com>
2026-07-16 11:34:43 -0400

    Formatting DriveIO.

--- src/main/java/com/team236/frc2026/subsystems/drive/DriveIO.java
+++ src/main/java/com/team236/frc2026/subsystems/drive/DriveIO.java
@@ -1,21 +1,17 @@
 package com.team236.frc2026.subsystems.drive;
 
-import java.util.function.Supplier;
-
-import org.littletonrobotics.junction.AutoLog;
-
-import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
-
+import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.AutoLog;
 
 /**
- * DriveIO interface sets up the input and output systems for the drive train.
- * Contains methods to control and monitor drive train.
+ * DriveIO interface sets up the input and output systems for the drive train. Contains methods to
+ * control and monitor drive train.
  */
-
 public interface DriveIO {
 
     @AutoLog
@@ -26,7 +22,7 @@ public interface DriveIO {
             this.Pose = new Pose2d(); // Later abstract this out
         }
 
-        public void setFromState (SwerveDriveState currentState) {
+        public void setFromState(SwerveDriveState currentState) {
             this.Pose = currentState.Pose;
             this.SuccessfulDaqs = currentState.SuccessfulDaqs;
             this.FailedDaqs = currentState.FailedDaqs;
