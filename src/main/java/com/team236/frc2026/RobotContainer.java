cdc2039816fd1868d41c16a051bf05cac4213899
OwenGarabedian <owengarabedian@gmail.com>
2026-07-17 09:00:55 -0400

    Added controller, drivesubsystem, and teleop swerve command to robot container.

--- src/main/java/com/team236/frc2026/RobotContainer.java
+++ src/main/java/com/team236/frc2026/RobotContainer.java
@@ -1,3 +1,32 @@
 package com.team236.frc2026;
 
-public class RobotContainer {}
+import com.team236.frc2026.commands.TeleopSwerveDrive;
+import com.team236.frc2026.subsystems.drive.DriveHardware;
+import com.team236.frc2026.subsystems.drive.DriveSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class RobotContainer {
+
+    public static final XboxController driverController =
+            new XboxController(Constants.Controller.kMainController);
+
+    private DriveSubsystem buildDriveSubsystem() {
+        return new DriveSubsystem(
+                new DriveHardware(
+                        mRobotState,
+                        Constants.DriveConstants.kDrivetrain.getDrivetrainConstants(),
+                        Constants.DriveConstants.kDrivetrain.getModuleConstants()));
+    }
+
+    private final DriveSubsystem mDriveSubsystem = buildDriveSubsystem();
+    private final RobotState mRobotState = new RobotState();
+
+    private final TeleopSwerveDrive driveCommand =
+            (new TeleopSwerveDrive(
+                    mDriveSubsystem,
+                    mRobotState,
+                    this,
+                    () -> driverController.getLeftX(),
+                    () -> driverController.getLeftY(),
+                    () -> driverController.getRightX()));
+}
