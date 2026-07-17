41c0e6b79ab8707bdd4f986790150655fd0c21ab
OwenGarabedian <owengarabedian@gmail.com>
2026-07-17 08:21:05 -0400

    Added alliance function to robot state.

--- src/main/java/com/team236/frc2026/RobotState.java
+++ src/main/java/com/team236/frc2026/RobotState.java
@@ -1,5 +1,13 @@
 package com.team236.frc2026;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import java.util.Optional;
+
 public class RobotState {
 
+    public boolean isRedAlliance() {
+        return DriverStation.getAlliance().isPresent()
+                && DriverStation.getAlliance().equals(Optional.of(Alliance.Red));
+    }
 }
