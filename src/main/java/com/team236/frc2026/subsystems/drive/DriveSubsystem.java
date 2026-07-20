49431546d0319cd2034a575c45310a1a6be6a5d3
OwenGarabedian <owengarabedian@gmail.com>
2026-07-20 10:21:26 -0400

    Added: All logging has been added to the periodic method.

--- src/main/java/com/team236/frc2026/subsystems/drive/DriveSubsystem.java
+++ src/main/java/com/team236/frc2026/subsystems/drive/DriveSubsystem.java
@@ -1,7 +1,9 @@
 package com.team236.frc2026.subsystems.drive;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.team236.lib.time.RobotTime;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 /**
  * The {@code DriveSubsystem} class controls the swerve drivetrain for both manual and autonomous
@@ -17,9 +19,12 @@ public class DriveSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
+        double timestamp = RobotTime.getTimestampSeconds();
+
         mIo.readInputs(mInputs);
-        // Logger.processInputs("DriveInputs", mInputs);
+        Logger.processInputs("DriveInputs", mInputs);
         mIo.logModules(mInputs);
+        logDriveSubsystem(timestamp);
     }
 
     public void setControl(SwerveRequest request) {
@@ -28,5 +33,13 @@ public class DriveSubsystem extends SubsystemBase {
 
     public void resetGyro() {
         mIo.resetGyro();
+    }
+
+    public void logDriveSubsystem(double timestamp) {
+        Logger.recordOutput(
+                "Drive/latencyPeriodicSec", RobotTime.getTimestampSeconds() - timestamp);
+        Logger.recordOutput(
+                "Drive/currentCommand",
+                (getCurrentCommand() == null) ? "Default" : getCurrentCommand().getName());
     }
 }
