54181d44a40eed5e22077666abdefecb1bfb9618
OwenGarabedian <owengarabedian@gmail.com>
2026-07-20 23:44:58 -0400

    Added: Constants can now check which robot is running code. Does this through checking for Mac adress in network.

--- src/main/java/com/team236/frc2026/Constants.java
+++ src/main/java/com/team236/frc2026/Constants.java
@@ -10,6 +10,9 @@ package com.team236.frc2026;
 import com.team236.frc2026.subsystems.drive.DrivetrainProfile;
 import com.team236.frc2026.subsystems.drive.PracTunerConstants;
 import edu.wpi.first.wpilibj.RobotBase;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 /**
  * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
@@ -31,6 +34,9 @@ public final class Constants {
         REPLAY
     }
 
+    public static final String kPracticeBotMacAddress = "00:80:2F:40:FC:4A";
+    public static boolean kIsPracticeBot = hasMacAddress(kPracticeBotMacAddress);
+
     public static final class Controller {
         public static final byte kMainController = 0;
     }
@@ -43,5 +49,48 @@ public final class Constants {
 
         public static final DrivetrainProfile kDrivetrain =
                 PracTunerConstants.createDrivetrain(); // Add logic later for comp + prac + sim
+    }
+
+    /**
+     * Check if this system has a certain mac address in any network device. (Taken from 254).
+     *
+     * @param mac_address Mac address to check (Uppercase with colons).
+     * @return true if some device with this mac address exists on this system.
+     */
+    public static boolean hasMacAddress(final String mac_address) {
+        try {
+            Enumeration<NetworkInterface> nwInterface = NetworkInterface.getNetworkInterfaces();
+            while (nwInterface.hasMoreElements()) {
+                NetworkInterface nis = nwInterface.nextElement();
+                if (nis == null) {
+                    continue;
+                }
+                StringBuilder device_mac_sb = new StringBuilder();
+                System.out.println("hasMacAddress: NIS: " + nis.getDisplayName());
+                byte[] mac = nis.getHardwareAddress();
+                if (mac != null) {
+                    for (int i = 0; i < mac.length; i++) {
+                        device_mac_sb.append(
+                                String.format("%02X%s", mac[i], (i < mac.length - 1) ? ":" : ""));
+                    }
+                    String device_mac = device_mac_sb.toString();
+                    System.out.println(
+                            "hasMacAddress: NIS "
+                                    + nis.getDisplayName()
+                                    + " device_mac: "
+                                    + device_mac);
+                    if (mac_address.equals(device_mac)) {
+                        System.out.println("hasMacAddress: ** Mac address match! " + device_mac);
+                        return true;
+                    }
+                } else {
+                    System.out.println("hasMacAddress: Address doesn't exist or is not accessible");
+                }
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
