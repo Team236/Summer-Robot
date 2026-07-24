9c1fd9a451755d3de295daf45e221a78b5cc73d1
OwenGarabedian <owengarabedian@gmail.com>
2026-07-24 09:40:02 -0400

    Added: Vision IO interface structure created.

---
+++ src/main/java/com/team236/frc2026/subsystems/vision/VisionIO.java
@@ -0,0 +1,13 @@
+package com.team236.frc2026.subsystems.vision;
+
+public interface VisionIO {
+
+    public static class VisionIOInputs {
+
+        class CameraIputs {
+
+        }
+
+        public CameraIputs cameraA;
+    }
+}
