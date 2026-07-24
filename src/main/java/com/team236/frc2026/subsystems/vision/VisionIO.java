e0da6348825afd7e9fa703db901e30c762379847
OwenGarabedian <owengarabedian@gmail.com>
2026-07-24 10:24:51 -0400

    Added: VisionIO has been built out, with one method that will be implimented in VisionHardware.

--- src/main/java/com/team236/frc2026/subsystems/vision/VisionIO.java
+++ src/main/java/com/team236/frc2026/subsystems/vision/VisionIO.java
@@ -1,13 +1,28 @@
 package com.team236.frc2026.subsystems.vision;
 
+import edu.wpi.first.math.geometry.Pose3d;
+
 public interface VisionIO {
 
+    // ** Inputs from vision system */
     public static class VisionIOInputs {
 
+        // ** Inputs from a single camera */
         class CameraIputs {
-
+            public boolean seesTag;
+            public FiducialObservation[] fiducialObservations;
+            public MegatagPoseEstimate megatagPoseEstimate;
+            public MegatagPoseEstimate megatag2PoseEstimate;
+            public int megatagCount;
+            public int megatag2Count;
+            public Pose3d pose3d;
+            public double[] standardDeviations = new double[12];
         }
 
-        public CameraIputs cameraA;
+        // Creation of Limelight camera
+        public CameraIputs cameraA = new CameraIputs();
     }
+
+    //Interface methods that must be implemented by VisionHardware
+    void readInputs(VisionIOInputs inputs);
 }
