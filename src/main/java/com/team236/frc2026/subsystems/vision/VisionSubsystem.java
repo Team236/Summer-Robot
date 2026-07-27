4135b338d2f636eb43c17adc659b1886ed8db9ab
OwenGarabedian <owengarabedian@gmail.com>
2026-07-27 10:40:03 -0400

    Style: Formatted for AOSP.

--- src/main/java/com/team236/frc2026/subsystems/vision/VisionSubsystem.java
+++ src/main/java/com/team236/frc2026/subsystems/vision/VisionSubsystem.java
@@ -1,53 +1,52 @@
 package com.team236.frc2026.subsystems.vision;
 
-import org.littletonrobotics.junction.Logger;
-
 import com.team236.frc2026.RobotState;
 import com.team236.lib.time.RobotTime;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 public class VisionSubsystem extends SubsystemBase {
-  private final VisionIO mIo;
-  private final RobotState mRobotState;
-  private final VisionIO.VisionIOInputs inputs = new VisionIO.VisionIOInputs();
+    private final VisionIO mIo;
+    private final RobotState mRobotState;
+    private final VisionIO.VisionIOInputs inputs = new VisionIO.VisionIOInputs();
 
-  public VisionSubsystem(VisionIO io, RobotState robotState) {
-    this.mIo = io;
-    this.mRobotState = robotState;
-  }
-
-  @Override
-  public void periodic() {
-    double startTime = RobotTime.getTimestampSeconds();
-    mIo.readInputs(inputs);
-
-    logCameraInputs("Vision/CameraA", inputs.cameraA);
-  }
-
-  private void logCameraInputs(String prefix, VisionIO.VisionIOInputs.CameraInputs camera) {
-    Logger.recordOutput(prefix, camera.seesTag);
-    Logger.recordOutput(prefix, camera.megatagCount);
-
-    if(DriverStation.isDisabled()) {
-        SmartDashboard.putBoolean(prefix + "/SeesTag", camera.seesTag);
-        SmartDashboard.putNumber(prefix + "/MegatagCount", camera.megatagCount);
+    public VisionSubsystem(VisionIO io, RobotState robotState) {
+        this.mIo = io;
+        this.mRobotState = robotState;
     }
 
-    if(camera.pose3d != null) {
-        Logger.recordOutput(prefix + "/Pose3d", camera.pose3d);
+    @Override
+    public void periodic() {
+        double startTime = RobotTime.getTimestampSeconds();
+        mIo.readInputs(inputs);
+
+        logCameraInputs("Vision/CameraA", inputs.cameraA);
     }
 
-    if(camera.megatagPoseEstimate != null) {
-        Logger.recordOutput(prefix + "/MegatagPoseEstimate", camera.megatagPoseEstimate.fieldToRobot());
-        Logger.recordOutput(prefix + "/Quality", camera.megatagPoseEstimate.quality());
-        Logger.recordOutput(prefix + "/AvgTagArea", camera.megatagPoseEstimate.avgTagArea());
-    }
+    private void logCameraInputs(String prefix, VisionIO.VisionIOInputs.CameraInputs camera) {
+        Logger.recordOutput(prefix, camera.seesTag);
+        Logger.recordOutput(prefix, camera.megatagCount);
 
-    if(camera.fiducialObservations != null) {
-        Logger.recordOutput(prefix + "/FiducialCount", camera.fiducialObservations.length);
+        if (DriverStation.isDisabled()) {
+            SmartDashboard.putBoolean(prefix + "/SeesTag", camera.seesTag);
+            SmartDashboard.putNumber(prefix + "/MegatagCount", camera.megatagCount);
+        }
+
+        if (camera.pose3d != null) {
+            Logger.recordOutput(prefix + "/Pose3d", camera.pose3d);
+        }
+
+        if (camera.megatagPoseEstimate != null) {
+            Logger.recordOutput(
+                    prefix + "/MegatagPoseEstimate", camera.megatagPoseEstimate.fieldToRobot());
+            Logger.recordOutput(prefix + "/Quality", camera.megatagPoseEstimate.quality());
+            Logger.recordOutput(prefix + "/AvgTagArea", camera.megatagPoseEstimate.avgTagArea());
+        }
+
+        if (camera.fiducialObservations != null) {
+            Logger.recordOutput(prefix + "/FiducialCount", camera.fiducialObservations.length);
+        }
     }
-  }
 }
