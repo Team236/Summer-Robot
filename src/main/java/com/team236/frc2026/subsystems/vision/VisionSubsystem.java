92e7973d06333a5b45d8fdc13c158f2d5f77ae9a
OwenGarabedian <owengarabedian@gmail.com>
2026-07-27 10:35:10 -0400

    Added: Created new method for logging camera inputs.

--- src/main/java/com/team236/frc2026/subsystems/vision/VisionSubsystem.java
+++ src/main/java/com/team236/frc2026/subsystems/vision/VisionSubsystem.java
@@ -1,8 +1,12 @@
 package com.team236.frc2026.subsystems.vision;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.team236.frc2026.RobotState;
 import com.team236.lib.time.RobotTime;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
@@ -17,5 +21,33 @@ public class VisionSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    double startTime = RobotTime.getTimestampSeconds();
+    mIo.readInputs(inputs);
+
+    logCameraInputs("Vision/CameraA", inputs.cameraA);
+  }
+
+  private void logCameraInputs(String prefix, VisionIO.VisionIOInputs.CameraInputs camera) {
+    Logger.recordOutput(prefix, camera.seesTag);
+    Logger.recordOutput(prefix, camera.megatagCount);
+
+    if(DriverStation.isDisabled()) {
+        SmartDashboard.putBoolean(prefix + "/SeesTag", camera.seesTag);
+        SmartDashboard.putNumber(prefix + "/MegatagCount", camera.megatagCount);
+    }
+
+    if(camera.pose3d != null) {
+        Logger.recordOutput(prefix + "/Pose3d", camera.pose3d);
+    }
+
+    if(camera.megatagPoseEstimate != null) {
+        Logger.recordOutput(prefix + "/MegatagPoseEstimate", camera.megatagPoseEstimate.fieldToRobot());
+        Logger.recordOutput(prefix + "/Quality", camera.megatagPoseEstimate.quality());
+        Logger.recordOutput(prefix + "/AvgTagArea", camera.megatagPoseEstimate.avgTagArea());
+    }
+
+    if(camera.fiducialObservations != null) {
+        Logger.recordOutput(prefix + "/FiducialCount", camera.fiducialObservations.length);
+    }
   }
 }
