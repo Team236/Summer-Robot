8f24c6708ebf3d81043ae032ed3d778bd72caaef
OwenGarabedian <owengarabedian@gmail.com>
2026-08-07 11:07:07 -0400

    Refactor: Reorganized the methods and abstracted the updating logic to its own mehtod.

--- src/main/java/com/team236/frc2026/subsystems/vision/VisionHardwareLimelight.java
+++ src/main/java/com/team236/frc2026/subsystems/vision/VisionHardwareLimelight.java
@@ -3,6 +3,7 @@ package com.team236.frc2026.subsystems.vision;
 import com.team236.frc2026.Constants.VisionConstants;
 import com.team236.frc2026.RobotState;
 import com.team236.lib.limelight.LimelightHelpers;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
@@ -14,14 +15,18 @@ public class VisionHardwareLimelight implements VisionIO {
 
     private NetworkTable mTableA =
             NetworkTableInstance.getDefault().getTable(VisionConstants.kLimelightAName);
-    private RobotState mRobotState;
-    private static int errCount;
-
     private static final double[] kDefaultStd = new double[VisionConstants.kStdDevArrayLength];
+    private RobotState mRobotState;
+    private static int mErrCount;
 
     public VisionHardwareLimelight(RobotState robotState) {
         this.mRobotState = robotState;
         setLLConfig();
+    }
+
+    @Override
+    public void readInputs(VisionIOInputs ioInputs) {
+        readCameraData(mTableA, ioInputs.cameraA, VisionConstants.kLimelightAName);
     }
 
     private void setLLConfig() {
@@ -42,41 +47,31 @@ public class VisionHardwareLimelight implements VisionIO {
         camInputs.seesTag = LimelightHelpers.getTV(limelightName);
 
         if (camInputs.seesTag) {
-            try {
-                var megatag = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
-                var megatag2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
-                var robotPose3d =
-                        LimelightHelpers.toPose3D(
-                                LimelightHelpers.getBotPose_wpiBlue(limelightName));
-
-                if (megatag != null) {
-                    camInputs.megatagPoseEstimate = MegatagPoseEstimate.fromLimelight(megatag);
-                    camInputs.megatagCount = megatag.tagCount;
-                    camInputs.fiducialObservations =
-                            FiducialObservation.fromLimelight(megatag.rawFiducials);
-                }
-                if (megatag2 != null) {
-                    camInputs.megatag2PoseEstimate = MegatagPoseEstimate.fromLimelight(megatag2);
-                    camInputs.megatag2Count = megatag2.tagCount;
-                }
-                if (robotPose3d != null) {
-                    camInputs.pose3d = robotPose3d;
-                }
-                camInputs.standardDeviations =
-                        table.getEntry("stddevs").getDoubleArray(kDefaultStd);
-            } catch (Exception err) {
-                if (errCount < 1) {
-                    System.err.println("Error processing vision data: " + err.getMessage());
-                    errCount++;
-                } else {
-                    return;
-                }
-            }
+            updatePoseEstimates(table, camInputs, limelightName);
         }
     }
 
-    @Override
-    public void readInputs(VisionIOInputs ioInputs) {
-        readCameraData(mTableA, ioInputs.cameraA, VisionConstants.kLimelightAName);
+    private void updatePoseEstimates(
+            NetworkTable table, VisionIOInputs.CameraInputs camInputs, String limelightName) {
+        try {
+            var megatag = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+            var megatag2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+
+            if (megatag != null) {
+                camInputs.megatagPoseEstimate = MegatagPoseEstimate.fromLimelight(megatag);
+                camInputs.megatagCount = megatag.tagCount;
+                camInputs.pose3d = new Pose3d(megatag.pose);
+            }
+            if (megatag2 != null) {
+                camInputs.megatag2PoseEstimate = MegatagPoseEstimate.fromLimelight(megatag2);
+                camInputs.megatag2Count = megatag2.tagCount;
+            }
+
+            camInputs.standardDeviations = table.getEntry("stddevs").getDoubleArray(kDefaultStd);
+        } catch (Exception err) {
+            System.err.printf(
+                    "Error # %d reading Limelight data: %s%n", mErrCount, err.getMessage());
+            mErrCount++;
+        }
     }
 }
