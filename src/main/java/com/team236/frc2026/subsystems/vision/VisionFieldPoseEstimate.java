3982d3d61515f321713768cf1ee1af6eebf58f17
OwenGarabedian <owengarabedian@gmail.com>
2026-08-09 14:04:52 -0400

    Added: This class holds vision pose estimate data specific to fusing data, and added data to the "addVisionMeasurement()" method.

---
+++ src/main/java/com/team236/frc2026/subsystems/vision/VisionFieldPoseEstimate.java
@@ -0,0 +1,40 @@
+package com.team236.frc2026.subsystems.vision;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+
+public class VisionFieldPoseEstimate {
+    private final Pose2d mVisionRobotPose;
+    private final double mTimestampSeconds;
+    private final Matrix<N3, N1> mVisionMeasurementStdDevs;
+    private final int mTagCount;
+
+    public VisionFieldPoseEstimate(
+            Pose2d visionRobotPose,
+            double timestamp,
+            Matrix<N3, N1> visionMeasurementStdDevs,
+            int tagCount) {
+        this.mVisionRobotPose = visionRobotPose;
+        this.mTimestampSeconds = timestamp;
+        this.mVisionMeasurementStdDevs = visionMeasurementStdDevs;
+        this.mTagCount = tagCount;
+    }
+
+    public Pose2d getVisionRobotPose() {
+        return mVisionRobotPose;
+    }
+
+    public double getTimestampSeconds() {
+        return mTimestampSeconds;
+    }
+
+    public Matrix<N3, N1> getvisionMeasurementStdDevs() {
+        return mVisionMeasurementStdDevs;
+    }
+
+    public int getTagCount() {
+        return mTagCount;
+    }
+}
