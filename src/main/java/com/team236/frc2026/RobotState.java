76b5356e5f5077e36a68220bbc954a3079f7984b
OwenGarabedian <owengarabedian@gmail.com>
2026-08-09 20:40:53 -0400

    Added: Method for retreaving last pose update from vision data. Solves NT time inconsistancies.

--- src/main/java/com/team236/frc2026/RobotState.java
+++ src/main/java/com/team236/frc2026/RobotState.java
@@ -5,9 +5,14 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import java.util.Optional;
 
 public class RobotState {
+    public double lastUsedMegatagTimestamp = 0;
 
     public boolean isRedAlliance() {
         return DriverStation.getAlliance().isPresent()
                 && DriverStation.getAlliance().equals(Optional.of(Alliance.Red));
+    }
+
+    public double getLastUsedMegatagTimestamp() {
+        return lastUsedMegatagTimestamp;
     }
 }
