08025a0258a2201eb1f53346a862853c45caf99a
Owen Garabedian <Owengarabedian@gmail.com>
2026-08-11 15:50:18 -0400

    Added: This is a buffer class that is thread safe. It allows data specifically pose2d's in this case to be saved and then looked back at.

--- src/main/java/com/team236/frc2026/RobotState.java
+++ src/main/java/com/team236/frc2026/RobotState.java
@@ -1,11 +1,22 @@
 package com.team236.frc2026;
 
+import com.team236.lib.robot.ConcurrentTimeInterpolatableBuffer;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import java.util.Optional;
 
 public class RobotState {
     public double lastUsedMegatagTimestamp = 0;
+    public final double kLogBackTime = 1.0;
+
+    private final ConcurrentTimeInterpolatableBuffer<Pose2d> fieldToRobot =
+            ConcurrentTimeInterpolatableBuffer.createBuffer(kLogBackTime);
+
+
+    public RobotState(){
+
+    }
 
     public boolean isRedAlliance() {
         return DriverStation.getAlliance().isPresent()
