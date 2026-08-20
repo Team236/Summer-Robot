13f8dbae731ca48965d4696395a40289a7da6c10
OwenGarabedian <owengarabedian@gmail.com>
2026-08-20 16:33:18 -0400

    Added: configured fuel physic sim as discribed in chief delfi.

--- src/main/java/com/team236/frc2026/Robot.java
+++ src/main/java/com/team236/frc2026/Robot.java
@@ -9,6 +9,8 @@ package com.team236.frc2026;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.team236.lib.limelight.Limelight3GConfig;
+import com.team236.lib.simulation.FuelPhysicsSim;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.RobotBase;
@@ -130,7 +132,19 @@ public class Robot extends LoggedRobot {
 
     /** This function is called once when the robot is first started up. */
     @Override
-    public void simulationInit() {}
+    public void simulationInit() {
+        FuelPhysicsSim ballSim = new FuelPhysicsSim("Sim/Fuel");
+        ballSim.enable();
+        ballSim.placeFieldBalls();  // spawns all the game pieces
+
+        // tell it about your robot
+        // ballSim.configureRobot(34, 34, 10,
+        //     () -> mRobotState.getPose(), () -> swerve.getChassisSpeeds());
+
+        // in simulationPeriodic()
+        ballSim.tick();  // runs physics, publishes ball positions to NT
+
+    }
 
     /** This function is called periodically whilst in simulation. */
     @Override
