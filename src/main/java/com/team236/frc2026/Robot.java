1bba2d171b94ecc7d6be9a0dc339bcf7d3082d02
OwenGarabedian <owengarabedian@gmail.com>
2026-08-22 11:07:58 -0400

    Refactor: Changed to using the configuration method created in fuel physics sim.

--- src/main/java/com/team236/frc2026/Robot.java
+++ src/main/java/com/team236/frc2026/Robot.java
@@ -2,6 +2,7 @@ package com.team236.frc2026;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.team236.lib.limelight.Limelight3GConfig;
+import com.team236.lib.simulation.FuelPhysicsSim;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.RobotBase;
@@ -125,14 +126,30 @@ public class Robot extends LoggedRobot {
     /** This function is called once when the robot is first started up. */
     @Override
     public void simulationInit() {
-        if (Constants.kUseMapleSim) {
+        if (Constants.kUseMapleSim && Constants.kUseMapleSimFuel) {
             SimulatedArena.getInstance().placeGamePiecesOnField();
+        }
+
+        if (!Constants.kUseMapleSimFuel) {
+            FuelPhysicsSim.configureFuelSim(
+                    Constants.TestbedConstants.kBumperWidthInches,
+                    Constants.TestbedConstants.kBumperLengthInches,
+                    Constants.TestbedConstants.kBumperHeightInches,
+                    () -> mRobotContainer.getSimulatedRobotState().getLatestFieldToRobot(),
+                    () ->
+                            mRobotContainer
+                                    .getDriveSubsystem()
+                                    .getMapleSimDrivetrain()
+                                    .mapleSimDrive
+                                    .getDriveTrainSimulatedChassisSpeedsFieldRelative());
         }
     }
 
     /** This function is called periodically whilst in simulation. */
     @Override
     public void simulationPeriodic() {
+        if (!Constants.kUseMapleSimFuel) {}
+
         Logger.recordOutput(
                 "Sim/Fuel", SimulatedArena.getInstance().getGamePiecesArrayByType("Fuel"));
     }
